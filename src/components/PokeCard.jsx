0d3fb5e3dae4c334e8ffe07136b50b2b19add41c
Glitch (glitch-hello-react) <none>
2021-08-22 18:52:16 +0000

    🚎🌂 Checkpoint ./src/components/PokeCard.jsx:16097079/672

--- src/components/PokeCard.jsx
+++ src/components/PokeCard.jsx
@@ -1,14 +1,17 @@
-import React from "react";
-import './pokecard.css'
-function PokeCard(props){
-    
-  
-  return(
-    <div className="pokecard">
-     {props.data}
-      </div> 
-    )
-
+import React, { Component } from "react";
+import "./pokecard.css";
+class PokeCard extends Component() {
+  constructor(props){
+  super()
+  this.state={
+    name:'',
+    imageUrl:'',
+    pokeIndex:''
+  }
+    }
+  render() {
+    return <div className="pokecard"><h1>{this.props.data}</h1></div>;
+  }
 }
 
 export default PokeCard;
