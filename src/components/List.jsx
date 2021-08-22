5f0b3134acde7805f0e59d1e771408a48168ab03
Glitch (glitch-hello-react) <none>
2021-08-22 18:16:20 +0000

    🏐🏬 Checkpoint ./src/components/List.jsx:16097079/4920

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -2,36 +2,43 @@ import React, { Component } from "react";
 import PokeCard from "./PokeCard.jsx";
 import "./list.css";
 import axios from "axios";
-var lista;
+
 class List extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       url: "https://pokeapi.co/api/v2/pokemon/", //https://pokeapi.co/api/v2/pokemon/?offset=20&limit=20
-      pokemon: null
+      pokemon: []
     };
   }
   async componentDidMount() {
     const resp = await axios.get(this.state.url).then(res => {
-      var pokes = res.data.results;
+      const pokes = res.data.results;
       this.setState({ pokemon: pokes });
       console.log(this.state.pokemon);
-      
     });
   }
-  
-  
-  if(pokemon) {
-    this.state.pokemon.forEach((i, poke) => {
-      return <PokeCard data={poke} />;
-    });
-  }
-  else
-  
+
   render() {
-    return <div className="list">{
-        Lista
-      }</div>;
+    var lista;
+
+    if (this.state.pokemon) {
+      lista = this.state.pokemon.forEach((poke, i) => {
+        console.log(poke);
+        return <PokeCard data={poke.names} />;
+      });
+    } else {
+      lista = <PokeCard data={"oi"}></PokeCard>;
+    }
+
+    return (
+      <div className="list">
+        {this.state.pokemon.forEach((poke, i) => {
+          console.log(poke.name);
+          return <PokeCard id={i} key={i} data={poke.name} />;
+        })}
+      </div>
+    );
   }
 }
 
