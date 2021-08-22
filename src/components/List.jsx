ae2c472edd89f3f96750338c37a6655a2205b03f
Glitch (glitch-hello-react) <none>
2021-08-22 18:05:13 +0000

    🍝🌲 Checkpoint ./src/components/List.jsx:16097079/6984

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,26 +1,37 @@
 import React, { Component } from "react";
 import PokeCard from "./PokeCard.jsx";
 import "./list.css";
-import axios from 'axios'
-
+import axios from "axios";
+var lista;
 class List extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
       url: "https://pokeapi.co/api/v2/pokemon/", //https://pokeapi.co/api/v2/pokemon/?offset=20&limit=20
       pokemon: null
     };
   }
   async componentDidMount() {
-    const resp = await axios.get(this.state.url)
-    .then((res)=>{
-      console.log(res.data,this.state.pokemon)
-      this.setState({pokemon: res.data.results})
-    })
-    }
-
+    const resp = await axios.get(this.state.url).then(res => {
+      var pokes = res.data.results;
+      this.setState({ pokemon: pokes });
+      console.log(this.state.pokemon);
+      
+    });
+  }
+  
+  
+  if(pokemon) {
+    this.state.pokemon.forEach((i, poke) => {
+      return <PokeCard data={poke} />;
+    });
+  }
+  else
+  
   render() {
-    return <div className="list"><PokeCard data = {this.state.pokemon.}/></div>;
+    return <div className="list">{
+        Lista
+      }</div>;
   }
 }
 
