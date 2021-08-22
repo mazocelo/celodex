6f6231fea4008335ebc5fef9029e60a0faa7247c
Glitch (glitch-hello-react) <none>
2021-08-22 17:18:02 +0000

    🎯👨 Checkpoint ./src/components/List.jsx:16097079/1214

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,22 +1,18 @@
-import React, {Component} from 'react'
-import PokeCard from './PokeCard.jsx'
-import './list.css'
+import React, { Component } from "react";
+import PokeCard from "./PokeCard.jsx";
+import "./list.css";
 
-class List extends Component
-{
-  render(){
-    var card =[]
-    for(let  i = 0; i < 1000; i++){
-        card.push(<PokeCard/>)
-      }
-  return(
-
+class List extends Component {
+  state = {
+    url: "https://pokeapi.co/api/v2/ability/",
+    pokemon: null
+  };
+  componentDidMount() {
     
-    <div className='list'>
-    {      card   }
-        
- </div>
-      )
+    
+  }
+  render() {
+    return <div className="list">{card}</div>;
   }
 }
 
