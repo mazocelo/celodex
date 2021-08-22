20a1e0756cf7a6350c6e9c3b8e71f542355df85c
Glitch (glitch-hello-react) <none>
2021-08-22 17:53:11 +0000

    ⛰⏲ Checkpoint ./src/components/List.jsx:16097079/382

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -7,17 +7,20 @@ class List extends Component {
   constructor() {
     super()
     this.state = {
-      url: "https://pokeapi.co/api/v2/pokemon/",
+      url: "https://pokeapi.co/api/v2/pokemon/", //https://pokeapi.co/api/v2/pokemon/?offset=20&limit=20
       pokemon: null
     };
   }
   async componentDidMount() {
     const resp = await axios.get(this.state.url)
-    this.setState({pokemon: resp.data})
-  }
+    .then((res)=>{
+      console.log(res.data,this.state.pokemon)
+      this.setState({pokemon: res.data.results})
+    })
+    }
 
   render() {
-    return <div className="list"><PokeCard data = {this.state.pokemon}/></div>;
+    return <div className="list"><PokeCard data = {this.state.pokemon.}/></div>;
   }
 }
 
