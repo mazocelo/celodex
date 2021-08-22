41943d03fa150cd306aa9a4eaa04ab0ee5d4ccbc
Glitch (glitch-hello-react) <none>
2021-08-22 19:19:54 +0000

    🐩👡 Checkpoint ./src/components/PokeCard.jsx:16097079/2271

--- src/components/PokeCard.jsx
+++ src/components/PokeCard.jsx
@@ -1,20 +1,32 @@
 import React, { Component } from "react";
 import "./pokecard.css";
-class PokeCard extends Component{
-  constructor(props){
-  super(props)
-  this.state={
-    name:'',
-    imageUrl:'',
-    pokeIndex:''
+
+import axios from "axios";
+
+class PokeCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      imageUrl: "",
+      pokeIndex: ""
+    };
   }
-    }
+
+  async componetDidMount() {
+    const resp = await axios.get(this.props.url)
+    .then(res=>{
+      console.log(res.data)
+    })
+    
+  }
   render() {
-    return <div className="pokecard">
-      <img src= {this.props.url+''}/>        
-      <h1>{this.props.data}</h1>
-              
-    </div>;
+    return (
+      <div className="pokecard">
+        <img />
+        <h1>{this.props.data}</h1>
+      </div>
+    );
   }
 }
 
