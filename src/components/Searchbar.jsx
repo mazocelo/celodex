7c1ea561faa852680d42e17f7c6d1e333c09b4f1
Glitch (glitch-hello-react) <none>
2021-08-23 02:18:32 +0000

    🌵💘 Checkpoint ./src/components/Searchbar.jsx:16097079/1677

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,12 +1,18 @@
-import React, {Component} from 'react'
+import React, { Component } from "react";
 
-
-export default class Searchbar{
-  
-  render(){
-    return <div>
-      <input className='busca' type='text'></input>
-      <button className = 'btn-busca'type='submit'>Procurar</button>
-    </div>
+export default class Searchbar extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        {" "}
+        <input className="busca" type="text"></input>
+        <button className="btn-busca" type="submit">
+          Procurar
+        </button>
+      </div>
+    );
   }
 }
