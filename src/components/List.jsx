6b94b35093e2c148efec4f918e1cf0feba302946
Glitch (glitch-hello-react) <none>
2021-08-23 00:21:24 +0000

    🏑🙈 Checkpoint ./src/components/List.jsx:16097079/3467

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -8,7 +8,7 @@ class List extends Component {
     super(props);
     this.state = {
       offset: 0,
-      limit: 21,
+      limit: 20,
       url: "https://pokeapi.co/api/v2/pokemon/?offset=00&limit=1080",
       pokemon: [],
       lista: []
@@ -24,11 +24,11 @@ class List extends Component {
       .then(res => {
         const pokes = res.data.results;
         const listados = new Array(this.state.limit);
-        
+
         const contagem = this.state.offset + this.state.limit;
-        
+
         this.setState({ pokemon: pokes });
-        });
+      });
   }
 
   more21(e) {
@@ -39,32 +39,23 @@ class List extends Component {
   }
 
   render() {
-     var novaLista = [];
-     var pokes = this.state.pokemon  
-     const start = new Array(this.state.limit)
-        start.map((cada,i)=>{
-          console.log(pokes[i])
-          novaLista.push(pokes[i])
-        //this.setState({lista: novaLista })  
-        })
-        console.log(novaLista);;
-       
-    
-    
     return (
       <div className="list">
-        {this.state.lista.map((poke, i) => {
-          console.log(poke);
-          if(i > this.state.limit){
+        {this.state.pokemon.map((poke, i) => {
+          //console.log(poke);
           
-          }else{
+          if (i < this.state.limit && i >this.state) {
+            
+          } else {
+            return (
+              <PokeCard key={i} url={poke.url} data={poke.name}></PokeCard>
+            );
           }
-          
         })}
         <button
           className="btnMore"
           onClick={e => {
-            this.emore21(e);
+            this.more21(e);
           }}
         >
           {">>"}
